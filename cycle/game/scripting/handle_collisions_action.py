f750f66e37fc29d18eebbb967ae86da08e699722
calvinwins <weaver95@byui.edu>
2022-03-17 19:37:47 -0500

    handle

--- cycle/game/scripting/handle_collisions_action.py
+++ cycle/game/scripting/handle_collisions_action.py
@@ -9,7 +9,6 @@ class HandleCollisionsAction(Action):
     
     The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
     with the food, or the snake collides with its segments, or the game is over.
-
     Attributes:
         _is_game_over (boolean): Whether or not the game is over.
     """
@@ -24,7 +23,6 @@ class HandleCollisionsAction(Action):
 
     def execute(self, cast, script):
         """Executes the handle collisions action.
-
         Args:
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
